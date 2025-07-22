3abc5e1ac1c5d4db7740fe77123583082b68d639
Krish Verma <kishuverma2005@gmail.com>
2025-07-22 17:07:11 +0530

    Added BasicOperation.java with + , - , * , /
    This program performs basic arithmetic operations using two integers:
    - Addition
    - Subtraction
    - Multiplication
    - Division
    
    It helps understand operators and how they work in Java.

---
+++ BasicOperation.java
@@ -0,0 +1,16 @@
+public class BasicOperations {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        int sum = a + b;
+        int diff = a - b;
+        int product = a * b;
+        int division = a / b;
+
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + division);
+    }
+}
